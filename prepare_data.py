84eb40439b8313591fe9d27d4b9125aae1e68d33
VinhLoiIT <vinhloiit1327@gmail.com>
2020-05-09 11:06:56 +0700

    update prepare data for Bengali and Hindi

--- prepare_data.py
+++ prepare_data.py
@@ -54,5 +54,39 @@ def prepare_CEDAR(M: int, K: int, random_state=0, data_dir='data/CEDAR'):
     test_samples = list(map(get_path, test_samples))
     write_csv(os.path.join(data_dir, 'test.csv'), test_samples)
 
+
+def prepare_BHSig260(M: int, K: int, random_state=0, data_dir='data/BHSig260/Bengali'):
+    def get_path(row):
+        writer_id, x1, x2, y = row
+        if y == 1:
+            x1 = os.path.join(data_dir, f'{writer_id:03d}', f'B-S-{writer_id}-G-{x1:02d}.tif')
+            x2 = os.path.join(data_dir, f'{writer_id:03d}', f'B-S-{writer_id}-G-{x2:02d}.tif')
+        else:
+            x1 = os.path.join(data_dir, f'{writer_id:03d}', f'B-S-{writer_id}-G-{x1:02d}.tif')
+            x2 = os.path.join(data_dir, f'{writer_id:03d}', f'B-S-{writer_id}-F-{x2:02d}.tif')
+        return x1, x2, y # drop writer_id
+
+    random.seed(random_state)
+    signers = list(range(1, K+1))
+    num_genuine_sign = 24
+    num_forged_sign = 30
+
+    train_signers, test_signers = train_test_split(signers, test_size=K-M)
+    pair_genuine_genuine = list(itertools.combinations(range(1, num_genuine_sign+1), 2))
+    pair_genuine_forged = list(itertools.product(range(1, num_genuine_sign+1), range(1, num_forged_sign+1)))
+
+    train_samples = make_partition(train_signers, pair_genuine_genuine, pair_genuine_forged)
+    train_samples = list(map(get_path, train_samples))
+    write_csv(os.path.join(data_dir, 'train.csv'), train_samples)
+    test_samples = make_partition(test_signers, pair_genuine_genuine, pair_genuine_forged)
+    test_samples = list(map(get_path, test_samples))
+    write_csv(os.path.join(data_dir, 'test.csv'), test_samples)
+
 if __name__ == "__main__":
+    print('Preparing CEDAR dataset..')
     prepare_CEDAR(M=50, K=55)
+    print('Preparing Bengali dataset..')
+    prepare_BHSig260(M=50, K=100, data_dir='data/BHSig260/Bengali')
+    print('Preparing Hindi dataset..')
+    prepare_BHSig260(M=100, K=160, data_dir='data/BHSig260/Hindi')
+    print('Done')
